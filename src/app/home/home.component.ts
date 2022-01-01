02ed2872d094bb0b3b3b30bf4e176b7009e61c2e
Mightly Dragon <jingjie105@gmail.com>
2022-01-02 00:18:40 +0800

    Removed email feature. Added form for section 1 and checking validity of website inputted. Section 2 still not done

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit, ChangeDetectorRef  } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import {NgbModal, ModalDismissReasons, NgbModalOptions} from '@ng-bootstrap/ng-bootstrap';
+import { BotsService } from '../shared/bots.service';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  url_section_form = new FormGroup({
+    enter_website: new FormControl(''),
+  });
 
   automation_section_form = new FormGroup({
     
@@ -21,7 +26,10 @@ export class HomeComponent implements OnInit {
 
   selected_option= '';
 
-  constructor( private modalService: NgbModal, private cdRef:ChangeDetectorRef) {  this.modalOptions = {
+  bot_codes: string[] = [];
+  //bot_codes: string[] = ["await page.goto('https://touch.facebook.com/?_rdr', {waitUntil:'networkidle0',});", "await page.waitForSelector('input[name=email]'); await page.$eval('input[name=email]', el => el.value = 'jingjie105@hotmail.com');", "await page.$eval('input[id=m_login_password]', el => el.value = 'Gendensuikoden12!');"];
+
+  constructor( private modalService: NgbModal, private cdRef:ChangeDetectorRef, private botService: BotsService) {  this.modalOptions = {
     backdropClass:'customBackdrop'
   }}
 
@@ -32,7 +40,7 @@ export class HomeComponent implements OnInit {
 
   //Change btn background permanently when clicked.
   async selection_btn(selectionType: string){
-    console.log(selectionType);
+    //console.log(selectionType);
     this.automation_section_form.reset();
 
     
@@ -82,9 +90,40 @@ export class HomeComponent implements OnInit {
       return
     }
   }
-  add_code(){
-    console.log('hi');
-    console.log(this.automation_section_form.value);
+
+  //Section 1
+  add_code_website(val: any){
+    //console.log(val['enter_website']);
+
+    if(this.bot_codes.length >= 1){
+      console.log("Website edited");
+    }
+    
+    if(val['enter_website'] == null || val['enter_website'] == ""){
+      console.log("Empty input field");
+      return
+    }
+    let url: any;
+    try {
+      url = new URL(val['enter_website']);
+    } catch (error) {
+      console.log("Ensure that you copy the website exactly from its url.")
+      return
+    }
+   //"await page.goto('https://touch.facebook.com/?_rdr', {waitUntil:'networkidle0',});"
+    //console.log(url.hostname);
+    let code = "page.goto(" + "'" + url.hostname + "'" + ", {waitUntil: 'networkidle0',});";
+    //console.log(code);
+    this.bot_codes[0] = code;
+    console.log(this.bot_codes);
+    
+    
+  }
+
+  //Section 2
+  add_code(val: any){
+    console.log(val['enter_input']);
+    //console.log(this.selected_option);
   }
 
   Test(){
@@ -97,10 +136,6 @@ export class HomeComponent implements OnInit {
     }, (reason) => {
       console.log(reason);
     });
-  }
-
-  send_email(){
-    console.log("Email sent!");
   }
 
 }
